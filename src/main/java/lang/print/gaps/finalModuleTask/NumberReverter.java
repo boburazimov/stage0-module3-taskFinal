73a18e5ecf18891b24b56dab439b7787eee7ffcc
bobur <uzbek.4103@gmail.com>
2022-07-27 12:32:45 +0500

    stage0-module3-taskFinal v1.0

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,12 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+
+        int reverse = 0;
+        while (number != 0) {
+            reverse = reverse * 10 + number % 10;
+            number = number / 10;
+        }
+        System.out.println(reverse);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -2,5 +2,8 @@ package lang.print.gaps.finalModuleTask;
 
 public class TemperatureConverter {
     public void toFahrenheit(int temperatureCelsius){
+        // (30 °C × 9/5) + 32 = 86 °F
+        int fahrenheit = (temperatureCelsius * 9/5) + 32;
+        System.out.println(fahrenheit);
     }
 }
